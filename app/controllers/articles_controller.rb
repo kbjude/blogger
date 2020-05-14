6768a2b491d50697518d5cb1b1c10725afc8566f
kbjude <kbjude@gmail.com>
2020-05-14 23:16:39 +0300

    delete successfully implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,28 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+   @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article = Article.destroy(params[:id])
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
