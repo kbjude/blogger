0968ed86cb682b14a66bd6d3f19d162e865ae27d
kbjude <kbjude@gmail.com>
2020-05-15 00:46:18 +0300

    update done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,17 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
   private
 
   def article_params
