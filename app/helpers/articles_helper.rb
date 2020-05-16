d963b1334c2f254a9165e2b50db8b14d5b4fe996
kbjude <kbjude@gmail.com>
2020-05-16 21:38:39 +0300

    Adding images and addition of paper clip

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,5 @@
 module ArticlesHelper
     def article_params
-        params.require(:article).permit(:title, :body, :tag_list)
-    end
-    
+        params.require(:article).permit(:title, :body, :tag_list, :image, :image)
+    end 
 end
